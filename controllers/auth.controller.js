c0d6d291421c438db58dba63d6f4817be4902a92
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-03-29 15:26:43 +0700

    Add login google

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,15 @@
+
+const { OK } = require("../core/responses/success.response")
+const AuthService = require('../services/auth.service')
 class AuthController {
     async logIn(req,res) {
     
+    }
+    async logInGoogle(req,res) {
+        new OK({
+            message: 'Log in successfully',
+            metadata: await AuthService.logInGoogle({data:req})
+        }).send(res)
     }
 }
 module.exports = new AuthController()
--- services/auth.service.js
+++ services/auth.service.js
@@ -2,5 +2,8 @@ class AuthService {
     static logIn = async ({}) => {
 
     }
+    static logInGoogle = async ({data}) => {
+        console.log(data);
+    }
 }
 module.exports = AuthService
