0301f4c59d79719fafa4cf82b88a2d8fb2ecaf48
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-04-14 10:23:11 +0700

    add swagger

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,10 @@ const { OK, CREATED } = require("../core/responses/success.response")
 const AuthService = require('../services/auth.service')
 class AuthController {
     async logIn(req,res) {
-    
+        new OK({
+            message: 'Log in successfully',
+            metadata: await AuthService.logIn(req.body)
+        }).send(res)
     }
     async logInGoogle(req,res) {
         console.log(req.user);        
