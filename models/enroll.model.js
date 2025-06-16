107b3d9d71687940b24b173dedb8b7bfc7f6cbcd
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-06-16 23:58:25 +0700

    fix: import file

---
+++ models/enroll.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const isDeleteSchema = require("./isDelete.model");
+
+const EnrollmentSchema = new mongoose.Schema({
+     user: {
+          type: mongoose.Schema.Types.ObjectId, 
+          ref: "User", required: true 
+     },
+     course: {
+          type: mongoose.Schema.Types.ObjectId, 
+          ref: "Course"
+     },
+     progress: {
+          type: Number, 
+          default: 0 
+     },
+     completed: {
+          type: Boolean, 
+          default: false 
+     },
+     enrolledAt: {
+          type: Date, 
+          default: Date.now 
+     },
+     fullName: { 
+          type: String 
+     },
+     email: { 
+          type: String 
+     },
+     phone: { 
+          type: String 
+     },
+     note: { 
+          type: String 
+     },
+     isConsulted: { 
+          type: Boolean, 
+          default: false 
+     },
+    ...isDeleteSchema.obj
+});
+
+module.exports = mongoose.model("Enrollment", EnrollmentSchema);
